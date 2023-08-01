05f996c0850098a35ffa403c0726f98a4b1578cc
RichardFrangenberg <richard@prism-pipeline.com>
2023-08-01 23:10:57 +0100

    fix PATH

--- pxr/__init__.py
+++ pxr/__init__.py
@@ -1,4 +1,9 @@
 __all__ = ['Tf', 'Gf', 'Trace', 'Work', 'Plug', 'Vt', 'Ar', 'Kind', 'Sdf', 'Ndr', 'Sdr', 'Pcp', 'Usd', 'UsdGeom', 'UsdVol', 'UsdMedia', 'UsdShade', 'UsdLux', 'UsdProc', 'UsdRender', 'UsdHydra', 'UsdRi', 'UsdSkel', 'UsdUI', 'UsdUtils', 'UsdPhysics', 'UsdAbc', 'UsdMtlx', 'Garch', 'CameraUtil', 'PxOsd', 'Glf', 'UsdImagingGL', 'UsdAppUtils', 'Usdviewq', 'UsdBakeMtlx']
 import os
-os.add_dll_directory(os.path.dirname(__file__) + "/bin")
-os.add_dll_directory(os.path.dirname(__file__) + "/lib")
+
+bin_path = os.path.dirname(__file__) + "/bin"
+lib_path = os.path.dirname(__file__) + "/lib"
+os.add_dll_directory(bin_path)
+os.add_dll_directory(lib_path)
+os.environ["PATH"] = os.environ["PATH"] + os.pathsep + bin_path
+os.environ["PATH"] = os.environ["PATH"] + os.pathsep + lib_path
